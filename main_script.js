bfe5d5a37a8bff1ae86647c538c07151273b3617
l.cs.dorina <l.cs.dorina@gmail.com>
2023-12-19 12:18:11 +0100

    talanok

--- main_script.js
+++ main_script.js
@@ -1,7 +1,12 @@
 ///////////////////////////////////////////////////
-document.addEventListener('DOMContentLoaded', function() {
-    const text = document.querySelector('.hello-szoveg');
-    setTimeout(function() {
-      text.style.opacity = '0';
-    }, 3000); 
+document.addEventListener('DOMContentLoaded', function () {
+  const elem = document.querySelector('.hello-szoveg')
+  elem.style.backgroundColor = "rgb(119,69,39)";
+  elem.style.margin = "12px";
+  elem.style.padding = "20px";
+  elem.style.borderRadius ="10px"
+  const text = document.querySelector('.hello-szoveg');
+  setTimeout(function () {
+    text.style.opacity = '0';
+  }, 3000);
 });
