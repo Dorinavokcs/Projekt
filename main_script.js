caa74c515d0d60b89630e894af8fb0c2286c5068
GyetvaiAdam <adamgyetvai26@gmail.com>
2024-01-15 22:04:20 +0100

    egész javascript megvalósitása

--- main_script.js
+++ main_script.js
@@ -1,9 +1,11 @@
 function hattervaltozas(){
     var body = document.body;
     var szin = body.style.backgroundColor;
+    const main = document.querySelector(".maincontainer");
 
-    if (szin !== 'grey'){
+    if (szin != 'grey'){
         body.style.backgroundColor = 'grey';
+        main.style.backgroundColor = `rgba(${251},${120},${120},${.9})`;
     }else{
         body.style.backgroundColor = '';
     }
