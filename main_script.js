03d6797c50b3cda8ef76d57e1432734d33e2c1d1
l.cs.dorina <l.cs.dorina@gmail.com>
2024-01-16 21:28:18 +0100

    talanok

--- main_script.js
+++ main_script.js
@@ -1,25 +1,12 @@
-<<<<<<< HEAD
-function hattervaltozas(){
-    var body = document.body;
-    var szin = body.style.backgroundColor;
 
-    if (szin !== 'grey'){
-        body.style.backgroundColor = 'grey';
-    }else{
-        body.style.backgroundColor = '';
-    }
+function hattervaltozas() {
+  const body = document.body;
+  const szin = body.style.backgroundColor;
+  const main = document.querySelector(".maincontainer");
+  if (szin != 'grey') {
+    body.style.backgroundColor = 'grey';
+    main.style.backgroundColor = `rgba(${251},${120},${120},${.9})`;
+  } else {
+    body.style.backgroundColor = '';
+  }
 }
-=======
-///////////////////////////////////////////////////
-document.addEventListener('DOMContentLoaded', function () {
-  const elem = document.querySelector('.hello-szoveg')
-  elem.style.backgroundColor = "rgb(119,69,39)";
-  elem.style.margin = "12px";
-  elem.style.padding = "20px";
-  elem.style.borderRadius ="10px"
-  const text = document.querySelector('.hello-szoveg');
-  setTimeout(function () {
-    text.style.opacity = '0';
-  }, 3000);
-});
->>>>>>> bfe5d5a37a8bff1ae86647c538c07151273b3617
