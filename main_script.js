f6d0a4dc44c1f23399ed8d5e0f4f6d5b62b03ed5
R4bbitzzzz <128032035+R4bbitzzzz@users.noreply.github.com>
2024-01-23 19:05:53 +0100

    Update main_script.js

--- main_script.js
+++ main_script.js
@@ -1,12 +1,20 @@
-function hattervaltozas(){
-    var body = document.body;
-    var szin = body.style.backgroundColor;
-    const main = document.querySelector(".maincontainer");
+ const gomb = document.getElementsByClassName("gomb")
+ for (let i = 0; i < gomb.length; i++) {
+   gomb[i].addEventListener("click", hattervaltozas);
+ }
 
-    if (szin != 'grey'){
-        body.style.backgroundColor = 'grey';
-        main.style.backgroundColor = `rgba(${251},${120},${120},${.9})`;
-    }else{
-        body.style.backgroundColor = '';
-    }
+function hattervaltozas() {
+  const body = document.body;
+  const szin = body.style.backgroundColor;
+  const main = document.querySelector(".maincontainer");
+  if (szin != 'grey') {
+    body.style.backgroundColor = 'grey';
+    main.style.backgroundColor = `rgba(${251},${120},${120},${.9})`;
+  } else {
+    body.style.backgroundColor = '';
+  }
+}
+function hideText() {
+  var textElement = document.getElementById('animation');
+  textElement.style.opacity = 0;
 }
